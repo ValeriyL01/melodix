3ae3f8720ec68489ee1c7d6ce213c75085193180
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-10-25 10:23:36 +0300

    fix: changed the callback component

--- src/app/pages/Callback.tsx
+++ src/app/pages/Callback.tsx
@@ -1,13 +1,28 @@
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { clientId, getAccessToken } from "../api/auth";
 
 const Callback: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
+
   useEffect(() => {
     const token = localStorage.getItem("access_token");
+
     if (token) {
       navigate("/", { replace: true });
+    } else {
+      const handleCallback = async () => {
+        const params = new URLSearchParams(location.search);
+        const code = params.get("code");
+
+        if (code) {
+          await getAccessToken(clientId, code);
+          navigate("/", { replace: true });
+        }
+      };
+
+      handleCallback();
     }
   }, [location, navigate]);
 
